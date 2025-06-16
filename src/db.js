8a4c178d1299f4c9e1f6721fda0bcf4cb3c3a4e4
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2025-06-16 09:49:24 -0300

    remove ssl db

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 import { Pool } from 'pg';
+import 'dotenv/config';
 
 const isLocal = process.env.DATABASE_URL && process.env.DATABASE_URL.includes('localhost');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isLocal ? false : { rejectUnauthorized: true }
+  ssl: isLocal ? false : { rejectUnauthorized: false }, // Aceita certificados autoassinados no Render
 });
 
 pool.on('connect', () => console.log('Connected to PostgreSQL database'));
